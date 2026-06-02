8d362c5ef9e6c1f353d9c6dc036639cf1d5163bc
CoinBR <coinbr@gmail.com>
2026-06-02 11:57:04 -0300

    telescope: find all files by default
    not only git changed files

--- lua/coinbr/plugins/telescope.lua
+++ lua/coinbr/plugins/telescope.lua
@@ -5,14 +5,19 @@ return {
   config = function()
 
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find files in git repo' })
-    vim.keymap.set('n', '<leader>ctrlp', builtin.git_files, { desc = 'Find files in git repo' })
+    vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Find files in git project' })
+    vim.keymap.set('n', '<leader>ctrlp', builtin.find_files, { desc = 'Find files in project' })
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Find files in project' })
+
     vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = 'Find files in git repo' })
 
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Find files in project' })
     vim.keymap.set('n', '<leader>ps', function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") });
     end, {desc = "Search text in project"})
+
+    vim.keymap.set('n', '<C-S-f>', function()
+      builtin.live_grep()
+    end, { desc = 'Search text in project' })
 
   end
 }
